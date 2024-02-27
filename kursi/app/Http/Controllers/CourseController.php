7179b3616c864c6871c0519169c6d528a7aaf2b6
Dimitrijevs <githubodinlv@gmail.com>
2024-02-27 14:11:41 +0200

    final

---
+++ kursi/app/Http/Controllers/CourseController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    //
+
+    public function index()
+    {
+        $courses = Course::all();
+
+        return response()->json($courses);
+    }
+
+
+    public function store(Request $request){
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'baner' => 'required|url',
+            'address' => 'required',
+            'people_number' => 'required|integer|min:0',
+        ]);
+
+        Course::create($validatedData);
+    }
+}
